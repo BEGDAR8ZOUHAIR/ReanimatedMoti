60651972f57f43e24ba9f83c0fef000f79cdff49
BEGDAR8ZOUHAIR <begdar8zouhair@gmail.com>
2023-10-19 10:02:43 +0100

    setup

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </View>
+  );
+}
+
