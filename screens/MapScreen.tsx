6ad535044345207d51181b5cbe90903901d0de37
BEGDAR8ZOUHAIR <begdar8zouhair@gmail.com>
2023-10-25 10:08:48 +0100

    feat building

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -1,31 +1,52 @@
-import react from "react";
-import { Text, View, StyleSheet } from "react-native";
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View } from 'react-native';
 import Mapbox from '@rnmapbox/maps';
-
+import * as Location from 'expo-location';
 Mapbox.setAccessToken('sk.eyJ1IjoiYmVnZGFyOHpvdWhhaXIiLCJhIjoiY2xvNHdpeTByMDVvYTJsbXZxMDRyeGl4ZiJ9.EVStFwIsctTv7WeUVeOPww');
 
-
 export default function MapScreen() {
+    const [location, setLocation] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                console.error('Permission to access location was denied');
+                return;
+            }
+            const currentLocation = await Location.getCurrentPositionAsync({});
+            setLocation(currentLocation.coords);
+        })();
+    }, []);
     return (
-        <View style={styles.page}>
-            <View style={styles.container}>
-                <Mapbox.MapView style={styles.map} />
-                <Text>Map</Text>
-            </View>
+        <View style={styles.container}>
+            {location && (
+                <Mapbox.MapView style={styles.map} styleURL={Mapbox.StyleURL.Street}>
+                    <Mapbox.Camera
+                        zoomLevel={15}
+                        centerCoordinate={[location.longitude, location.latitude]}
+                        animationMode="flyTo"
+                        animationDuration={2000}
+                    />
+
+                    <Mapbox.PointAnnotation
+                        id="userLocation"
+                        coordinate={[location.longitude, location.latitude]}
+                        title="Your location"
+                    />
+                </Mapbox.MapView>
+            )}
         </View>
     )
 };
 
 const styles = StyleSheet.create({
-    page: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
     container: {
-        height: 300,
-        width: 300,
+        flex: 1,
+
+        backgroundColor: '#fff',
     },
+
     map: {
         flex: 1,
     },
